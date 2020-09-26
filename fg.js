06cca60bc1a7373a118e2604863177b25e7f0d38
paradiseMaestro <korotkih1liya@yandex.ru>
2020-09-26 16:55:07 +0300

    go1 create a version

--- fg.js
+++ fg.js
@@ -1,39 +1,78 @@
 // let n = require('synaptic');
 // console.log(7556)
 
-let weight = 0.5;
+let weight = 0.5; // 0.5
 let arr;
-let sm = 0.0000001
+let sm = 0.00001 // 0.0000001
 let p_i = (input) =>{
     return input*weight
 }
 
 let r_i = (output) =>{
-    return output*weight
+    return output/weight
 }
-
+let corr;
 function train( inp, res){
     rezz = inp * weight;
     arr = res - rezz
-    let corr = (arr / rezz) *sm
+    // sm=1
+    corr = (arr / rezz) *sm
+
     weight += corr
+    // console.log("rezz "+ rezz);
+    // console.log("corr " + corr);
+    // console.log("arr "+arr);
+    // console.log("sm "+sm);
+    // console.log("weight "+weight);
+    // console.log("-------------------------------");
+    
 }
 
 i = 0;
 while(true){
     i++;
     train(100,62.1371)
-    // if(i==20000){
-        // break;
+
+    // if(corr<=5){
+        // console.log(corr);
+        // console.log(i);
+        // console.log("jkgjfgfffffffffffff");
+
     // }
+
+    if(i%100000==0){
+        console.log((arr > sm))
+        console.log((arr < -sm))
+        console.log("0000000000000000")
+    }
     if(!(arr > sm || arr < -sm)){
+    // console.log("arr "+arr);
+    // console.log("sm "+sm);
+    // console.log("sm ");
+
+    //     console.log((arr > sm))
+    //     console.log((arr < -sm))
+    //     console.log(i)
+    console.log("weight "+weight);
+
         break
     }
+
+
 }
+// console.log(!(arr > sm || arr < -sm))
+// console.log(arr < -sm)
+
 console.log(
 
 
     p_i(100),
     p_i(541)
-
+// 
 )
+    // console.log(
+
+        // (arr / rezz) * sm
+    // );
+    // console.log("rezz "+ rezz);
+    // console.log("arr "+arr);
